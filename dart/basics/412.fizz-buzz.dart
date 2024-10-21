81c6537f9b1bf4c313bd63954bc050f47504a897
Nicholas Kolatsis <nkolatsis@gmail.com>
2024-10-21 13:52:48 +0200

    Dart [412]

---
+++ dart/basics/412.fizz-buzz.dart
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=412 lang=dart
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+class Solution {
+  List<String> fizzBuzz(int n) {
+    List<String> result = [];
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        result.add('FizzBuzz');
+      } else if (i % 3 == 0) {
+        result.add('Fizz');
+      } else if (i % 5 == 0) {
+        result.add('Buzz');
+      } else {
+        result.add(i.toString());
+      }
+    }
+    return result;
+  }
+}
+// @lc code=end
